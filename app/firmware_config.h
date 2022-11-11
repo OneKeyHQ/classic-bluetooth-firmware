d9bc5f0dcd05ac119ed40ee36c14762eab8f0045
Adam BZH <424778940z@users.noreply.github.com>
2022-11-11 14:44:06 +0800

    move firmware info to seprate file
    Signed-off-by: Adam BZH <424778940z@users.noreply.github.com>

---
+++ app/firmware_config.h
@@ -0,0 +1,13 @@
+#ifndef FIRMWARE_CONFIG
+#define FIRMWARE_CONFIG
+
+#define MANUFACTURER_NAME               "OneKey"                                     /**< Manufacturer. Will be passed to Device Information Service. */
+#define ADV_HEAD_NAME                   "K"
+#define MODEL_NUMBER                    "one"                                       /**< Model Number string. Will be passed to Device Information Service. */
+#define MANUFACTURER_ID                 0x55AA55AA55                                /**< DUMMY Manufacturer ID. Will be passed to Device Information Service. You shall use the ID for your Company*/
+#define ORG_UNIQUE_ID                   0xEEBBEE                                    /**< DUMMY Organisation Unique ID. Will be passed to Device Information Service. You shall use the Organisation Unique ID relevant for your Company */
+#define HW_REVISION                     "1.0.0"
+#define FW_REVISION                     "1.3.0"
+#define SW_REVISION                     "s132_nrf52_7.0.1"
+
+#endif //FIRMWARE_CONFIG
